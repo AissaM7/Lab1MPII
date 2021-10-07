b44f62b7ff7850882ae28e45bf8b06f2954acc61
AissaM7 <92105506+AissaM7@users.noreply.github.com>
2021-10-07 11:24:41 -0400

    Create Lab1MP2.ino

--- Lab1MP2.ino
+++ Lab1MP2.ino
@@ -1,7 +1,7 @@
 //www.elegoo.com
 //2016.12.08
 
-int redPin = 11;
+int redPin = 11;// declaration of arduino pins
 int yellPin = 12;
 int greenPin = 13;
 int buttonApin = 9;
@@ -13,6 +13,7 @@ void setup() {
   pinMode(buttonApin, INPUT_PULLUP);  
   pinMode(yellPin,OUTPUT);
   pinMode(greenPin,OUTPUT);
+  
   pinMode(speaker,OUTPUT);
 }
 
@@ -22,9 +23,10 @@ void loop()
 {
 
 
-if (digitalRead(buttonApin) == HIGH){
+if (digitalRead(buttonApin) == HIGH){ //checks if button isnt pressed if not redlight just keeps blinking
+ 
    
-    digitalWrite(redPin,LOW);
+    digitalWrite(redPin,LOW);// Blinks the redlight before the sequence is started
     delay(350);
     digitalWrite(redPin,HIGH);
     delay(350);
@@ -33,32 +35,32 @@ if (digitalRead(buttonApin) == HIGH){
 
 }
   
-   if (digitalRead(buttonApin) == LOW)
+   if (digitalRead(buttonApin) == LOW) //checks if button pressed if it is it runs the loop
   {
 
     delay(500);
        
     while(digitalRead(buttonApin)==HIGH){
-    digitalWrite(redPin, HIGH);
+    digitalWrite(redPin, HIGH);//Turns the red light for 20 seconds 
     delay(17000);
-    digitalWrite(speaker,HIGH);
+    digitalWrite(speaker,HIGH);//speaker goes off for 1 seconds 3 seconds before light changes
     delay(1000);
-    digitalWrite(speaker,LOW);
+    digitalWrite(speaker,LOW);//speaker stops
     delay(2000);
     digitalWrite(redPin,LOW);
-    digitalWrite(greenPin,HIGH);
+    digitalWrite(greenPin,HIGH);//Green light goes off for 20 seconds
     delay(17000);
-    digitalWrite(speaker,HIGH);
+    digitalWrite(speaker,HIGH);//speaker goes off for 1 seconds 3 seconds before light changes
     delay(1000);
     digitalWrite(speaker,LOW);
     delay(2000);
     digitalWrite(greenPin,LOW);
-    digitalWrite(yellPin,HIGH);
-    digitalWrite(speaker,HIGH);
+    digitalWrite(yellPin,HIGH);//Yellow light goes off for 3 seconds
+    digitalWrite(speaker,HIGH);//speaker goes off for 1 seconds 3 seconds before light changes
     delay(1000);
     digitalWrite(speaker,LOW);
     delay(2000);
-    digitalWrite(yellPin,LOW);
+    digitalWrite(yellPin,LOW);//yellow light turns off
     
    }
     
