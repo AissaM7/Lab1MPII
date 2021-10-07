bd30b5fee7c2a9d2aba6bb968a2249c19cdfa0b5
Rsoto0525 <91100451+Rsoto0525@users.noreply.github.com>
2021-10-07 10:42:42 -0400

    Add files via upload

---
+++ Lab1MP2.ino
@@ -0,0 +1,70 @@
+//www.elegoo.com
+//2016.12.08
+
+int redPin = 11;
+int yellPin = 12;
+int greenPin = 13;
+int buttonApin = 9;
+int speaker=10;
+unsigned char i;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(buttonApin, INPUT_PULLUP);  
+  pinMode(yellPin,OUTPUT);
+  pinMode(greenPin,OUTPUT);
+  
+  pinMode(speaker,OUTPUT);
+}
+
+
+
+void loop() 
+{
+
+
+if (digitalRead(buttonApin) == HIGH){
+   
+    digitalWrite(redPin,LOW);
+    delay(350);
+    digitalWrite(redPin,HIGH);
+    delay(350);
+  
+    
+
+}
+  
+   if (digitalRead(buttonApin) == LOW)
+  {
+
+    delay(500);
+       
+    while(digitalRead(buttonApin)==HIGH){
+    digitalWrite(redPin, HIGH);
+    delay(17000);
+    digitalWrite(speaker,HIGH);
+    delay(1000);
+    digitalWrite(speaker,LOW);
+    delay(2000);
+    digitalWrite(redPin,LOW);
+    digitalWrite(greenPin,HIGH);
+    delay(17000);
+    digitalWrite(speaker,HIGH);
+    delay(1000);
+    digitalWrite(speaker,LOW);
+    delay(2000);
+    digitalWrite(greenPin,LOW);
+    digitalWrite(yellPin,HIGH);
+    digitalWrite(speaker,HIGH);
+    delay(1000);
+    digitalWrite(speaker,LOW);
+    delay(2000);
+    digitalWrite(yellPin,LOW);
+    
+   }
+    
+  
+  }
+
+
+}
